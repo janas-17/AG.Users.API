9f38077594fb4f51890c8af7d701e4e96d845b9f
janas-17 <59132747+janas-17@users.noreply.github.com>
2019-12-22 12:17:30 +0100

    Integration tests
    Controller integration tests with in memory DbContext and DB Seeder

---
+++ AG.Users.Tests/DatabaseSeeder.cs
@@ -0,0 +1,40 @@
+﻿using AG.Users.EFCore;
+using AG.Users.EFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AG.Users.Tests
+{
+    public static class DatabaseSeeder
+    {
+        public static void SeedTestData(UsersContext dbContext)
+        {
+            dbContext.Set<Operator>().Add(new Operator()
+            {
+                Approved = false,
+                DateOfBirth = new DateTime(2000, 1, 1),
+                FirstName = "John",
+                LastName = "Doe",
+                GameName = "Jdoe",
+            });
+            dbContext.Set<Operator>().Add(new Operator()
+            {
+                Approved = true,
+                DateOfBirth = new DateTime(1990, 6, 6),
+                FirstName = "Jane",
+                LastName = "Doe",
+                GameName = "jane",
+            });
+
+            dbContext.Set<Administrator>().Add(new Administrator()
+            {
+                DateOfBirth = new DateTime(2000, 1, 1),
+                FirstName = "Adam",
+                LastName = "Smith",
+            });
+
+            dbContext.SaveChanges();
+        }
+    }
+}
